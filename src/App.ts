fb9e4babbf492bd1333f3d2873c43aa899595d83
schngmg@gmail.com <saching@zone24x7.com>
2018-07-16 15:18:32 +0530

    impl of new routes

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,20 @@
 import * as express from 'express'
 import IssueController from './controllers/issue';
+import * as bodyParser from 'body-parser';
 class App {
-  public express: express;
+  public express;
   constructor() {
-    this.express = express()
+    this.express = express();
     this.mountRoutes()
   }
 
   private mountRoutes(): void {
     const router = express.Router();
-    router.get('/', IssueController.root)
-    this.express.use('/', router)
+    router.get('/', IssueController.root);
+    router.post('/createissue', IssueController.createIssue);
+    router.delete('/deleteissue/:issueid', IssueController.deleteIssue);
+    this.express.use(bodyParser());
+    this.express.use('/api/v1', router);
   }
 }
 
